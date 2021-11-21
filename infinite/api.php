8278867fa5bb2c2b8f2de9c296008935c1c6f44b
scelomgobhozi <67973295+scelomgobhozi@users.noreply.github.com>
2021-11-21 19:28:04 +0200

    Add files via upload

--- infinite/api.php
+++ infinite/api.php
@@ -6,8 +6,18 @@ $myRow = array();
 if(empty($_POST['iload'])){$vlimit = 1;}
 
 else{$vlimit = $_POST['iload'];}
+
+
+if(isset($_POST['oset']))
+{
+$oset = $_POST['oset'];
+}
+
+else{$oset = 0;}
+
+
 $myArray['vlimit'] = $vlimit;
-$query = "SELECT * FROM bpost ORDER BY id ASC LIMIT ".$vlimit." OFFSET 2";
+$query = "SELECT * FROM bpost ORDER BY id ASC LIMIT ".$vlimit." OFFSET ".$oset." ";
 $result = $link->query($query);
 while($row = $result->fetch_array())
 {
